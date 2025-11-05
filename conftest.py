73b0c7d1756bd7966b76c1b40c76c2de7e3d6c9b
Alexander Averin <a.averin@rosintel.com>
2025-11-05 13:54:36 +0300

    test ci cd

--- conftest.py
+++ conftest.py
@@ -27,6 +27,7 @@ def get_chrome_options():
 
     if config.HEADLESS:
         options.add_argument('--headless=new')
+        options.add_argument('--window-size=1920,1080')
 
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
